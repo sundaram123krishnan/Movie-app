48b00c6849249866acf64e269e5b60af0df9b19c
sundaram123krishnan <krishnsundaram@gmail.com>
2023-05-14 17:24:46 +0530

    added about section

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,12 @@
-export default function About(){
-  return <div>Hello About</div>
+
+
+'use client'
+
+import About from "../(components)/About.js";
+
+export default function AboutSection(){
+  return
+    <div>
+      <About/>
+    </div>
 }
