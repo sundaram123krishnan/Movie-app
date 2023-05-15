75d292c66d678ac0837f486ab49ade9c63bd12c7
sundaram123krishnan <krishnsundaram@gmail.com>
2023-05-15 14:54:57 +0530

    modified config for api images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images:{
+    domains:["occ-0-3933-116.1.nflxso.net"],
+  },
+}
 
 module.exports = nextConfig
