f351e445b01470af9bd3cad5ff601bc7f85dc596
sundaram123krishnan <krishnsundaram@gmail.com>
2023-05-15 14:57:23 +0530

    added movie prop

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,3 +1,28 @@
-export default function Movie(){
-  return <div>Hello i am from Movie</div>
+import MovieCard from "../(components)/MovieCard.js"
+
+export default async function Movie(){
+
+  const url = 'https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd3078dc112msh9be84be9abf208bp1ae2d4jsn2596c183f820',
+    'X-RapidAPI-Host': 'netflix54.p.rapidapi.com'
+  }
+};
+
+	const response = await fetch(url, options);
+	const result = await response.json();
+  const mainData = result.titles;
+  return(
+    <div>
+      <h1>Series and movies</h1>
+      {
+        mainData.map((item) => {
+          return <MovieCard key={item.id} {...item}/>
+        })
+
+      }
+    </div>)
 }
+
